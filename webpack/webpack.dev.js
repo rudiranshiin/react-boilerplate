55238bd0f5ef912c95f2805671fe56917e684fcd
Rudiyanto <rudi.yanto@go-jek.com>
2019-07-31 00:47:21 +0700

    webpack: add cli webpack dashboard for webpack dev server

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const common = require('./webpack.config');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 
 const HTML_DIR = path.resolve(__dirname, '../public/index.html');
 
@@ -10,7 +11,8 @@ const devConfig = merge(common, {
   plugins: [
     new HtmlWebpackPlugin({
       template: HTML_DIR
-    })
+    }),
+    new DashboardPlugin()
   ]
 });
 
